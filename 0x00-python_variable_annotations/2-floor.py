ae382a59e0f5f3fa40ccd30995b5bb611c1d946f
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-08 01:01:26 +0000

    Added Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module define a floor function
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """This module Round off a given float n to whole number
+
+    Arguments:
+    =========
+    n (float) - number to round off
+
+    Returns (integer) - result of rounding off n
+    """
+
+    return math.floor(n)
