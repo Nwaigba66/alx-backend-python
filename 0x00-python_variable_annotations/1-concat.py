22b825e7a83fe75a215a2ed6bae0f903ce89d736
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-08 00:50:29 +0000

    Added Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module define a concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This module Joins two strings together
+
+    Arguments:
+    =========
+    str1 (string) - string two
+    str2 (string) - string one
+
+    Returns: (concatenated string) result of joining str1 and str2
+    """
+    return str1 + stri2
