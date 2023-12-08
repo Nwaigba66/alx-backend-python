8e5f8931df8a3bff5ba86607aa31fa51dabc3772
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-08 01:13:42 +0000

    Added Basic annotations - to sting

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module define a function that convert float to string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """Convert float n to string representation
+
+    Arguments:
+    =========
+    n (float) - number to convert to string
+
+    Returns (string) - string representation of n
+    """
+    return "{}".format(n)
