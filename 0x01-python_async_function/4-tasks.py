3204dc8c0166c8a0c69d69f71b4d6058dd65dd9c
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-11 23:40:44 +0000

    Added more Tasks module with async

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""This module define a coroutine that call another coroutine n times
+"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Calls the wait_random n times
+
+    Arguments:
+    =========
+    n - number of calls of wait_random
+    max_delay - maximum delay per call of wwit_random
+    Returns - List of wait time for all calls
+    """
+    queue = []
+    for task in asyncio.as_completed(
+            [task_wait_random(max_delay) for idx in range(n)]):
+        ans = await task
+        queue.append(ans)
+    return queue
